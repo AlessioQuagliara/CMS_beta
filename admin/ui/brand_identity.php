c36b7c26b1929bda46544b69c7198e1774c0d174
AlessioSpotex <alessio.quagliara@spotexsrl.it>
2024-05-28 13:58:38 +0200

    update brand identity

--- admin/ui/brand_identity.php
+++ admin/ui/brand_identity.php
@@ -19,7 +19,6 @@ loggato()
     include '../materials/sidebar.php'; 
     ?>
 
-
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="container mt-5">
         <div class="p-3 mb-2 bg-light rounded-3 border d-flex justify-content-center align-items-center">
@@ -30,10 +29,12 @@ loggato()
         <h5>Carica le immagini</h5>
         <form action="../ui-gestisci/brand_caricamento.php" method="post" enctype="multipart/form-data">
             <div class="mb-3">
+                <img src="../../src/media_system/favicon_site.ico" alt="">
                 <label for="favicon" class="form-label">Carica Favicon (.ico)</label>
                 <input class="form-control" type="file" name="favicon" id="favicon" accept=".ico">
             </div>
             <div class="mb-3">
+                <img src="../../src/media_system/logo_site.png" alt="">
                 <label for="logo" class="form-label">Carica Logo (.png)</label>
                 <input class="form-control" type="file" name="logo" id="logo" accept=".png">
             </div>
